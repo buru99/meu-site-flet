fec4dc97d7d471c1e4ff19872885bda519b88fcc
buru99 <155699998+buru99@users.noreply.github.com>
2024-06-10 03:36:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "meu site flet"
-    page.bgcolor = ft.colors.RED_200
-    page.add(ft.Text("hello world"))
-    
-    
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-ft.app(target=main)
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+ft.app(main)
